688e83f317ada5ae40140ede156c7145b63513f2
Stephen Lammi <smlammi@gmail.com>
2023-04-21 13:25:19 -0400

    First clean version for initial cli/redis server interaction :) happy friday

--- pyredis.py
+++ pyredis.py
@@ -1,12 +1,21 @@
 import argparse
+import socket
+
+HOST = "127.0.0.1"  # Local host IP
+PORT = 6379  # Current port used by the Redis server
+SPACING = "\r\n"
+ARRAY_START = "*"
+BULK_STR_START = "$"
 
 
-def redis_test():
+def main():
+    # Start argparse for accepting commands from client
     parser = argparse.ArgumentParser(
         description="This will print a statement passed as argument",
         epilog="Why are you even reading the help page for this?",
     )
 
+    # required argument for 3 commands [GET, SET, DEL] and its proceeding argument providing keys to be retrieved, set or deleted.
     parser.add_argument(
         "message",
         type=str,
@@ -14,40 +23,47 @@ def redis_test():
         default="You didn't input anything",
     )
     parser.add_argument("key_or_value", type=str, nargs="+")
-    # parser.add_argument('--get','-g',nargs=1,type=str,help='Default get command. User input must be string')
-    # parser.add_argument('--set','-s',type=str, help='Default set command. User input must be string')
-    # parser.add_argument('--del','-d',help='Default delete command.')
+
+    # Potential future command when working with the need to change ports:
+
     # parser.add_argument('--port','-p',type=int,default=6379, help='This is the default port to connect to')
+
+    # Generates redis formatted array from CLI input.
     args = parser.parse_args()
-    print(args.message)
-    print(args.key_or_value)
-    print(len(args.key_or_value))
-    input_to_length = {item: len(item) for item in args.key_or_value}
-    # print(input_to_length)
-    # print(args.port)
-    # print(args.get)
+    redis_format_client_message = generate_redis_formated_array(
+        args.key_or_value, args.message
+    )
 
-    key_or_value_num = len(args.key_or_value)
-    command_length = len(args.message)
+    # Opens connection with server and facilities send/recieve of client message/server response.
+    server_connect_and_communicate(redis_format_client_message)
+
+
+def generate_redis_formated_array(key_or_value, cli_message):
+    # Creates dictionary that stores strings of user input with their character length
+    # (also a string) for generation of bulk strings and array to be passed to Redis server.
+    input_to_length = {item: len(item) for item in key_or_value}
+    key_or_value_num = len(key_or_value)
+    command_length = len(cli_message)
     total_length = 1 + key_or_value_num
-    print(total_length)
-    spacing = "\r\n"
-    message_start = f"*{total_length}\r\n${command_length}\r\n"
+    encoded_message_start = f"{ARRAY_START}{total_length}{SPACING}{BULK_STR_START}{command_length}{SPACING}{cli_message}{SPACING}"
 
-    # print(get_length)
-    client_encode = f"{message_start}"
+    # Creates and returns redis formatted array to send to Redis server
+    client_encode = encoded_message_start
     for key, value in input_to_length.items():
-        client_encode = client_encode + f"${value}\r\n{key}\r\n"
-    print(client_encode)
-
-    # f"*{total_length}\r\n${command_length}\r\n{args.message}\r\n${argument_length}"
-    # pyredis.py GET hello world
-    # *3\r\n$3\r\nGET\r\n$5\r\n\hello\r\n$5\r\nworld\r\n
-    # print(client_encode)
+        client_encode += f"{BULK_STR_START}{value}{SPACING}{key}{SPACING}"
+    redis_format_client_message = client_encode
+    return redis_format_client_message
 
 
-# redis_test("hello world")
+def server_connect_and_communicate(redis_formatted_cli_message):
+    # Function that will open connection with server on specificied HOST and PORT, transmits encoded message,
+    # and provides server response. Message must be encoded into Redis format before passing to this function.
+    with socket.socket() as s:
+        s.connect((HOST, PORT))
+        s.sendall(redis_formatted_cli_message.encode())
+        response = s.recv(1024).decode()
+        print(f"Recieved {repr(response)}")
 
 
 if __name__ == "__main__":
-    redis_test()
+    main()
