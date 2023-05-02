3fc11b62dad5e5fff97dced1650ba1b2d5c2bd8a
Stephen Lammi <smlammi@gmail.com>
2023-05-02 16:55:52 -0400

    Created command handler for server/dictionary. SET still needs time based options. Bug when entering same argument twice ie: GET x x

--- redis-server.py
+++ redis-server.py
@@ -1,23 +1,51 @@
 # redis-server.py
 
 import socket
+import argparse
 
-from utils import server_response_decode
+from utils import server_response_decode, command_handler
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65430  # Port to listen on (non-privileged ports are > 1023)
+PORT = 65429  # Port to listen on (non-privileged ports are > 1023)
 MY_DICT = {}
+FAKE_RESPONSE_1 = "OK"
+FAKE_RESPONSE_2 = ""
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    while True:
-        conn, addr = s.accept()
-        print(f"Connected by {addr}")
-        data = conn.recv(1024).decode()
-        if not data:
-            break
-        decoded_response, _ = server_response_decode(data)
-        str_decoded_response = " ".join(decoded_response)
-        # parse command and either retrieve/store data or return error message
-        conn.sendall(str_decoded_response.encode())
+
+# Creates socket object that binds port and listens for incoming connection.
+# Returns raw decoded data if needed (for debugging ect.)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--port",
+        "-p",
+        type=int,
+        default=6379,
+        help="This is the default port to connect to",
+    )
+    return parser.parse_args()
+
+
+def listen_and_respond():
+    args = parse_args()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, args.port))
+        s.listen()
+        # Creates infinite while statement that accepts incoming connections and transmits data between
+        while True:
+            conn, addr = s.accept()
+            print(f"Connected by {addr}")
+            data = conn.recv(1024).decode()
+            # if no data is sent the statements are broken and script ends.
+            if not data:
+                break
+            decoded_response, _ = server_response_decode(data)
+            # str_decoded_response = " ".join(decoded_response)
+            # parse command and either retrieve/store data or return error message
+            RESP_encoded_response = command_handler(decoded_response, MY_DICT)
+            conn.sendall(f"{RESP_encoded_response}".encode())
+    return data
+
+
+if __name__ == "__main__":
+    listen_and_respond()
--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
 SEPARATOR = "\r\n"
+ARRAY_START = "*"
+BULK_STR_START = "$"
+BUFFER_SIZE = 1024
+ARRAY_ERROR_MESSAGE = "Additional input required"
+# GET_TOO_MUCH_INPUT_ERROR = "-ERR unknown command" f"{message[2:]}"
+MY_DICT = {}
+NIL_REPLY = "$-1\r\n"
+OKAY = "+OK\r\n"
+PORT = 65429
+
+import argparse, socket
 
 
 def server_response_decode(message):
@@ -7,7 +18,7 @@ def server_response_decode(message):
     message_parts = message[1:].split(SEPARATOR, maxsplit=1)
 
     # Array if statement start
-    if message[0] == "*":
+    if message[0] == ARRAY_START:
         array_len = int(message_parts[0])
         array_list = []
 
@@ -23,17 +34,17 @@ def server_response_decode(message):
         return array_list, remainder
 
     elif message[0] == "+":
-        print("simple string")
+        # print("simple string")
         value = message_parts[0]
         remainder = message_parts[1]
 
     elif message[0] == "-":
-        print("error")
-        value = message_parts[0]
+        # print("error")
+        value = f"(error) {message_parts[0]}"
         remainder = message_parts[1]
 
-    elif message[0] == "$":  # remember nil responses are sent as $-1
-        print("bulk string")
+    elif message[0] == BULK_STR_START:  # remember nil responses are sent as $-1
+        # print("bulk string")
         b_string_len = int(message_parts[0])
         if b_string_len == -1:
             value = "(nil)"
@@ -43,7 +54,85 @@ def server_response_decode(message):
             remainder = message_parts[1][b_string_len + 2 :]
 
     elif message[0] == ":":  # responses should start with '(integer)'
-        print("integer")
-        value = int(message_parts[0])
+        # print("integer")
+        value = f"(integer) {message_parts[0]}"
         remainder = message_parts[1]
     return value, remainder
+
+
+def generate_redis_formatted_array(key_or_value, cli_message):
+    # Creates dictionary that stores strings of user input with their character length
+    # (also a string) for generation of bulk strings and array to be passed to Redis server.
+    input_to_length = {item: len(item) for item in key_or_value}
+    command_length = len(cli_message)
+    total_length = 1 + len(key_or_value)
+    encoded_message_start = f"{ARRAY_START}{total_length}{SEPARATOR}{BULK_STR_START}{command_length}{SEPARATOR}{cli_message}{SEPARATOR}"
+
+    # Creates and returns redis formatted array to send to Redis server
+    for key, value in input_to_length.items():
+        encoded_message_start += f"{BULK_STR_START}{value}{SEPARATOR}{key}{SEPARATOR}"
+    return encoded_message_start
+
+
+def command_handler(message, dict):
+    # generates bulk string, simple string, null reply and integer reply.
+    if type(message) != list:
+        return f"-ERR unkown command {*message,}{SEPARATOR}"
+    if message[0] == "GET":
+        if len(message) > 2:
+            return f"-ERR unkown command {*message[2:],}{SEPARATOR}"
+        elif message[1] not in dict:
+            return NIL_REPLY
+        elif message[1] in dict:
+            value_len = len(dict[message[1]])
+            return f"${value_len}{SEPARATOR}{dict[message[1]]}{SEPARATOR}"
+    if message[0] == "DEL":
+        count = 0
+        del_len = len(message) - 1
+        for i in range(del_len):
+            if message[i + 1] in dict:
+                del dict[message[i + 1]]
+                count += 1
+        return f":{count}{SEPARATOR}"
+    if message[0] == "SET":
+        if len(message) >= 4:
+            if message[3] == "GET":
+                if message[1] in dict:
+                    old_key_len = len(dict[message[1]])
+                    old_key = dict[message[1]]
+                    old_key_BS = f"${old_key_len}{SEPARATOR}{old_key}{SEPARATOR}"
+                    dict.update({message[1]: message[2]})
+                    return old_key_BS
+                else:
+                    dict[message[1]] = message[2]
+                    return NIL_REPLY
+            if message[3] == "NX":
+                if message[1] in dict:
+                    return NIL_REPLY
+                else:
+                    dict[message[1]] = message[2]
+                    return OKAY
+            if message[3] == "XX":
+                if message[1] in dict:
+                    dict.update({message[1]: message[2]})
+                    return OKAY
+                else:
+                    return NIL_REPLY
+        elif len(message) == 3:
+            if message[1] in dict:
+                dict.update({message[1]: message[2]})
+                return OKAY
+            else:
+                dict[message[1]] = message[2]
+                return OKAY
+    return f"-ERR unknown command '{' '.join(message)}'{SEPARATOR}"
+
+
+# this will likely need to be updated as other message types arise, however given
+# that command_handler already converts most of replies into RESP format this is not needed currently
+# may consider transfering this part into command_handler if other types dont arise.
+def server_response_encode(cmd_hndlr_message):
+    if type(cmd_hndlr_message) == int:
+        return f":{cmd_hndlr_message}\r\n"
+    else:
+        return cmd_hndlr_message
